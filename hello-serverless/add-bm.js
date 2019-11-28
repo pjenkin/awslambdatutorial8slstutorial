d64a740f80fbc146f3333b21a93856e314a7e011
Peter Jenkin <peternjenkin@gmail.com>
2019-11-28 16:24:55 +0000

    8-173 HTTP body mapping template and Lambda Integration added, also with Postman settings

--- hello-serverless/add-bm.js
+++ hello-serverless/add-bm.js
@@ -1,9 +1,12 @@
 'use strict';
 
-// 8-165 Creating APIs using the Serverless Farmework
+// 8-165 Creating APIs using the Serverless Framework
+// 8-171 Lambda Integration & Body Mapping Templates with Serverless Framework
+// Request body variables will be mapped to js variables ...
+// ... the Request body mapping template must be written in the function definition in serverless.yml (qv)
 
-module.exports.add = async event => {
-
+module.exports.add = async (event, context) => {
+/*
     let {num1, num2} = JSON.parse(event.body);
     // NB pattern/syntax for initialising using event in Lambda Functions
     let output = {
@@ -11,11 +14,16 @@ module.exports.add = async event => {
         num2: num2,
         result: num1 + num2
     }
-
+    console.log(output);    // just in case of a problem
     return {
     statusCode: 200,
     body: JSON.stringify(output),
   };
+*/
+    // REMmed code above from previous lectures
+    // look out for HTTP Request body, from which to map properties to js variables
+  let {num1, num2} = event;     // request body mapping, no need to parse
+  return num1 + num2;
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };

8220cd99455ed76615f359ff22028e8849cf1c32
Peter Jenkin <peternjenkin@gmail.com>
2019-11-28 15:23:57 +0000

    Corrected code and Postman check

---
+++ hello-serverless/add-bm.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// 8-165 Creating APIs using the Serverless Farmework
+
+module.exports.add = async event => {
+
+    let {num1, num2} = JSON.parse(event.body);
+    // NB pattern/syntax for initialising using event in Lambda Functions
+    let output = {
+        num1: num1,
+        num2: num2,
+        result: num1 + num2
+    }
+
+    return {
+    statusCode: 200,
+    body: JSON.stringify(output),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
